b8895de422a2af50ce4de609d66ef703570700b2
geekfan <geekfan@brucedeMacBook-Pro.local>
2017-12-09 01:54:14 +0800

    no message

--- honey-base.js
+++ honey-base.js
@@ -110,12 +110,12 @@ var HURL = {
 	GetURLParam:function(param){
 		paramValue = "";
 	    isFound = false;
-	    if(this.location.search.indexOf("?") == 0 && this.location.search.indexOf("=") > 1) {
-	        arrSource = unescape(this.location.search).substring(1, this.location.search.length).split("&");
+	    if(window.location.search.indexOf("?") == 0 && window.location.search.indexOf("=") > 1) {
+	        arrSource = unescape(window.location.search).substring(1, window.location.search.length).split("&");
 	        i = 0;
 	        while(i < arrSource.length && !isFound){
 	            if(arrSource[i].indexOf("=") > 0) {
-	                if (arrSource[i].split("=")[0].toLowerCase() == paramName.toLowerCase()) {
+	                if (arrSource[i].split("=")[0].toLowerCase() == param.toLowerCase()) {
 	                    paramValue = arrSource[i].split("=")[1];
 	                    isFound = true;
 	                }
