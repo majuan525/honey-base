7dad7a7b3099f7d07cd3a4b4409f20e60c9f0911
geekfan <geekfan@brucedeMacBook-Pro.local>
2017-12-09 00:31:26 +0800

    no message

--- honey-base.js
+++ honey-base.js
@@ -1,5 +1,87 @@
-var Network = {
-	sum:function(a,b){
-		return a + b
+/**
+ * author:xxxx@gmail.com
+**/
+var HNetwork = {
+	PostWithRich:function(url,data,dataType,timeout,callback){
+		$.ajax({
+			 type:"POST",
+			 url:url,
+			 data: data,
+			 timeout:timeout,
+			 dataType:dataType,
+			 processData:true,
+			 success: function(msg){
+				 callback(msg);
+			 }
+		});
+	},
+	GetWithRich:function(url,data,dataType,timeout,callback){
+		$.ajax({
+			 type:"GET",
+			 url:url,
+			 data: data,
+			 timeout:timeout,
+			 dataType:dataType,
+			 processData:true,
+			 success: function(msg){
+				 callback(msg);
+			 }
+		});
+	},
+	PostAndReturnJson:function(url,data,callback){
+		$.ajax({
+			 type:"POST",
+			 url:url,
+			 data: data,
+			 timeout:180000,
+			 dataType:'json',
+			 processData:true,
+			 success: function(msg){
+				 callback(msg);
+			 }
+		});
+	},
+	Post:function(url,data,callback){
+		$.ajax({
+			 type:"POST",
+			 url:url,
+			 data: data,
+			 timeout:180000,
+			 processData:true,
+			 success: function(msg){
+				 callback(msg);
+			 }
+		});
+	},
+	Get:function(url,callback){
+		$.ajax({
+			 type:"GET",
+			 url:url,
+			 data: {},
+			 timeout:180000,
+			 processData:true,
+			 success: function(msg){
+				 callback(msg);
+			 }
+		});
+	},
+	GetJson:function(url,callback){
+		$.ajax({
+			 type:"POST",
+			 url:url,
+			 data: {},
+			 timeout:180000,
+			 dataType:'json',
+			 processData:true,
+			 success: function(msg){
+				 callback(msg);
+			 }
+		});
 	}
-}
+};
+var HStrings = {
+
+};
+var HRegex = {
+
+};
