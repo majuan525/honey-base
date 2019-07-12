fa0d69de1cf04d781db518a61aeaf3004c626c68
bruce <bruce.shaoheng@gmail.com>
2019-07-12 17:11:19 +0800

    init

--- honey.js
+++ honey.js
@@ -91,8 +91,12 @@ var HNetwork = {
 		           //     alert("您上传的文件格式有误，请重新选择！");  
 		           //     return false;  
 		           // }  
+		           console.log(file);
+		           console.log("size:" + getSize("hello"));
+		           console.log(document.getElementById("hello").files[0].size);
 		        },
 		        onComplete:function(file,resp) {  
+
 		           callback(file,resp);
 		        }
 			});
@@ -153,8 +157,57 @@ var HTool = {
 	            callback();
 	        }
 	    });
-	}
+	},
+	isPhone:function() {
+            var sUserAgent = navigator.userAgent.toLowerCase();
+            var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+            var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+            var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+            var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+            var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+            var bIsAndroid = sUserAgent.match(/android/i) == "android";
+            var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+            var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+            if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+                return true;
+            }else{
+                return false;
+            }
+        }
 };
+
+var HWindow = {
+     getAbsolutePosition:function (reference, target) {
+          var result = {
+              left: -target.clientLeft,
+              top: -target.clientTop
+          }
+          var node = target;
+          while(node != reference && node != document){
+             result.left = result.left + node.offsetLeft + node.clientLeft;
+             result.top = result.top + node.offsetTop + node.clientTop;
+             node = node.parentNode;
+         }
+         if(isNaN(reference.scrollLeft)){
+             result.right = document.documentElement.scrollWidth - result.left;
+             result.bottom = document.documentElement.scrollHeight - result.top;
+         }else {
+             result.right = reference.scrollWidth - result.left;
+             result.bottom = reference.scrollHeight - result.top;
+         }
+         return result;
+     },
+     getViewport:function (target) {
+         var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+         var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;
+         var absolutePosi = this.getAbsolutePosition(document, target);
+         var Viewport = {
+             left: absolutePosi.left - scrollLeft,
+             top: absolutePosi.top - scrollTop,
+         }
+         return Viewport;
+     }
+}
 
 
 
